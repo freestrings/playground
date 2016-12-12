61a61a9a6da8cf540f9419a05d1f8a1aaf4d8cc3
한창석 <freestrings@wemakeprice.com>
2016-12-12 15:32:57 +0900

    [rust-ds] lined-list, IterMut

--- rust-ds/src/linked_list.rs
+++ rust-ds/src/linked_list.rs
@@ -17,6 +17,10 @@ struct Iter<'a, T: 'a> {
     next: Option<&'a Node<T>>
 }
 
+struct IterMut<'a, T: 'a> {
+    next: Option<&'a mut Node<T>>
+}
+
 impl<'a, T> List<T> {
     fn new() -> Self {
         List {
@@ -60,11 +64,23 @@ impl<'a, T> List<T> {
     ///
     /// self.head.map(..) 은 borrow 에러. peek와 마찬가지로 as_ref로 참조 해야한다.
     /// 그래서 node dereferencing은 두번 `**` 한다.
-    /// 그리고 새 값은 head에 붙이기 때문에 iteration 순서는 거꾸로다.
+    /// 그리고,
+    /// - 새 값은 head에 붙이기 때문에 iteration 순서는 거꾸로다.
+    /// - `iter(&mut self)`, mutable self는 `mutable borrow`가 발생 하기 때문에, iter() 호출 이후 List에 immutable self 함수를 호출하면 문제가 생긴다.
+    ///    가령, `let mut iter = iter.iter()` 후 `list.peek()`는 mutable borrow가 되었기 때문에 immutable 호출 에러가 난다
     ///
-    fn iter(&mut self) -> Iter<T> {
+    fn iter(&self) -> Iter<T> {
         Iter {
             next: self.head.as_ref().map(|node| &**node)
+        }
+    }
+
+    ///
+    /// as_mut()!!
+    ///
+    fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut {
+            next: self.head.as_mut().map(|node| &mut **node)
         }
     }
 }
@@ -76,6 +92,24 @@ impl<'a, T> Iterator for Iter<'a, T> {
         self.next.map(|node| {
             self.next = node.next.as_ref().map(|node| &**node);
             &node.data
+        })
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    ///
+    /// as_mut()!!
+    ///
+    /// FIXME_1
+    /// take()!! take()을 하지 않으면 `cannot move out of borrowed content`에러가 난다. 그러면 lter_mut() 이후에는 list에 값이 없나?
+    /// 확인을 위해 list.iter_mut() 이후 list.peek()을 해 보려는데 테스트를 할 수 없다. 참고 tests->`FIXME_1`
+    ///
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_mut().map(|node| &mut **node);
+            &mut node.data
         })
     }
 }
@@ -123,6 +157,26 @@ mod tests {
         let mut iter = list.iter();
         assert_eq!(iter.next(), Some(&2));
         assert_eq!(iter.next(), Some(&1));
+
+        assert_eq!(list.peek(), Some(&2));
+    }
+
+    ///
+    /// FIXME_1
+    /// `fn iter(&self)`에 주석내용과 동일한 문제가 있다.
+    ///
+    /// `iter_mut`는 `&mut self`다. 그렇지만 list.peek는 &self 이기 때문에 mutable borrow(list.iter_mut()) 이후 immutable borrow(list.peek()) 했다는 에러가 난다.
+    ///
+    #[test]
+    fn test_iter_mut() {
+        let mut list = super::List::new();
+        list.push(1);
+        list.push(2);
+
+        let mut iter = list.iter_mut();
+
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
 
         assert_eq!(list.peek(), Some(&2));
     }
