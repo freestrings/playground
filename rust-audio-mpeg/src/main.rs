a2c6743bcca7b7467db05273d291267d2aff9885
freestrings <freestrings@gmail.com>
2017-04-21 18:36:40 +0900

    [rust-audio-mpeg] Root means square

--- rust-audio-mpeg/src/main.rs
+++ rust-audio-mpeg/src/main.rs
@@ -10,6 +10,20 @@ mod tests {
 
     use simplemad::{Decoder, Frame, MadFixed32};
 
+    ///
+    ///https://en.wikipedia.org/wiki/Root_mean_square
+    ///http://m.blog.naver.com/pkw00/220226903866
+    ///
+    fn to_rms(samples: &Vec<MadFixed32>) -> f64 {
+        let mut sum = 0_f64;
+
+        for sample in samples {
+            sum += sample.to_f64().powf(2.0);
+        }
+
+        (sum / (samples.len() as f64)).sqrt()
+    }
+
     #[test]
     fn t1() {
         let path = Path::new("resources/test.mp3");
@@ -17,22 +31,11 @@ mod tests {
         let decoder = Decoder::decode(file).unwrap();
 
         let samples: Vec<Vec<f64>> = decoder.filter_map(|r| match r {
-            Ok(f) => Some(f), 
-            _ => None
-        })
-        .map(|f| {
-            let samples = f.samples;
-
-            let mut l_sum = 0_f64;
-            for l_sample in &samples[0] {
-                l_sum += l_sample.to_f64();
-            }
-
-            let mut r_sum = 0_f64;
-
-            vec![l_sum, r_sum]
-        })
-        .collect();
+                Ok(f) => Some(f),
+                _ => None,
+            })
+            .map(|f| vec![to_rms(&f.samples[0]), to_rms(&f.samples[1])])
+            .collect();
 
         for sample in &samples {
             println!("{}", sample[0]);
