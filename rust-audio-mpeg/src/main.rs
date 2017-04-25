c504d80e9008066753089af1a9406e5475e6bf7a
freestrings <freestrings@gmail.com>
2017-04-25 18:35:34 +0900

    [rust-audio-mpeg] v2

--- rust-audio-mpeg/src/main.rs
+++ rust-audio-mpeg/src/main.rs
@@ -1,103 +1,184 @@
+#[macro_use]
 extern crate clap;
 extern crate image;
 extern crate imageproc;
 extern crate simplemad;
+extern crate term;
 
-use clap::App;
 use std::f64;
+use std::i64;
 use std::fs::File;
 
+use std::error::Error;
+
+use clap::App;
 use image::{Rgb, RgbImage};
 use imageproc::{drawing, rect};
 use simplemad::{Decoder, MadFixed32};
 
 ///https://en.wikipedia.org/wiki/Root_mean_square
 ///http://m.blog.naver.com/pkw00/220226903866
-fn to_rms(samples: &Vec<MadFixed32>) -> f64 {
+fn into_rms(samples: &Vec<MadFixed32>) -> f64 {
     let sum = samples.iter().fold(0.0, |_, sample| (sample.to_raw() as f64).powi(2));
     let len = samples.len() as f64;
     (sum / len).sqrt()
 }
 
-fn find_max(values: Vec<f64>) -> f64 {
-    let mut max = f64::MIN;
-    for v in values.iter() {
-        if max.max(*v) == *v {
-            max = *v;
-        }
-    }
-    max
-}
-
-fn samples(file: File) -> Vec<[f64; 2]> {
+fn samples(file: File) -> Vec<f64> {
     Decoder::decode(file)
         .unwrap()
         .filter_map(|r| match r {
             Ok(f) => Some(f),
             _ => None,
         })
-        .map(|f| [to_rms(&f.samples[0]), to_rms(&f.samples[1])])
+        .map(|f| [into_rms(&f.samples[0]), into_rms(&f.samples[1])]) //rms
+        .map(|s| (s[0] + s[1]) / 2.0) //mono
         .collect()
 }
 
+fn hexadecimal_to_decimal(hex: &str) -> u8 {
+    match i64::from_str_radix(hex, 16) {
+        Ok(v) => v as u8,
+        _ => 0,
+    }
+}
+
+fn hexcolor_to_rgb(hex: &str) -> Rgb<u8> {
+    let hex = if hex.starts_with("#") { &hex[1..] } else { hex };
+    let r = hexadecimal_to_decimal(&hex[0..2]);
+    let g = hexadecimal_to_decimal(&hex[2..4]);
+    let b = hexadecimal_to_decimal(&hex[4..6]);
+    Rgb([r, g, b])
+}
+
 fn main() {
+
     let matches = App::new("waveform")
         .version("0.1")
         .author("Changseok Han <freestrings@gmail.com>")
-        .args_from_usage("<INPUT>... 'mp3 file pathes. ex) ./waveform file1 file2'")
+        .args_from_usage(concat!("<INPUT>... 'mp3 file pathes. ex) ./waveform file1 file2'\n",
+                                 // width
+                                 "-w --width=[WIDTH] 'optional) a image width. the default \
+                                  width is 512 pixel'\n",
+                                 // height
+                                 "-h --height=[HEIGTH] 'optional) a image height. the default \
+                                  height is 120 pixel'\n",
+                                 // background color
+                                 "-b --background=[BACKGROUND] 'optional) a background hex \
+                                  color. the default value is #000000'\n",
+                                 // foreground color
+                                 "-f --foreground=[FOREGROUND] 'optional) a foreground hex \
+                                  color. the default value is #ffffff'\n",
+                                 "-o --output",
+                                 // verbose
+                                 "-v --verbose"))
         .get_matches();
 
-    let pathes:Vec<_> = matches.values_of("INPUT").unwrap().collect();
+    let mut terminal = term::stdout().unwrap();
 
-    for path in pathes.iter() {
+    let image_width = value_t!(matches.value_of("width"), u32).unwrap_or(512 as u32);
+    let image_height = value_t!(matches.value_of("height"), u32).unwrap_or(120 as u32);
+    //
+    // the color of canvas
+    //
+    let background = hexcolor_to_rgb(matches.value_of("background").unwrap_or("#000000"));
+    //
+    // the color of wave
+    //
+    let foreground = hexcolor_to_rgb(matches.value_of("foreground").unwrap_or("#ffffff"));
+
+    let pathes: Vec<_> = matches.values_of("INPUT").unwrap().collect();
+
+    let mut count = 0;
+    let total_pathes = pathes.len();
+
+    for path in pathes {
+
+        count += 1;
 
         match File::open(path) {
             Ok(file) => {
-                let samples: Vec<[f64; 2]> = samples(file);
+                let samples = samples(file);
+                let sample_len = samples.len();
+                let max_sample = samples.iter().cloned().fold(f64::MIN, f64::max);
 
-                let left_max = find_max(samples.iter().map(|s| s[0]).collect());
-                let right_max = find_max(samples.iter().map(|s| s[1]).collect());
+                let mut image = RgbImage::new(image_width, image_height);
 
-                let white = Rgb([255u8, 255u8, 255u8]);
+                drawing::draw_filled_rect_mut(&mut image,
+                                              rect::Rect::at(0 as i32, 0 as i32)
+                                                  .of_size(image_width as u32,
+                                                           image_height as u32),
+                                              background);
 
-                let image_height: usize = 120;
-                let image_width: usize = 512;
-                let half_height = image_height / 2;
-
-                let mut image = RgbImage::new(image_width as u32, image_height as u32);
-
-                let len = samples.len();
-                let ih = image_height as f64;
-
-                for n in 0..len {
-
-                    let left_sample = samples[n][0];
-                    let right_sample = samples[n][1];
-
-                    let half_y = left_sample * ih / left_max / 2.0;
-
-                    let x = (image_width * n / len) as i32;
-                    let y = (half_height as f64 - half_y) as i32;
-                    let w = 1 as u32;
-                    let h = (half_y * 2.0) as u32;
-
+                for n in 0..sample_len {
+                    let half_height = samples[n] * image_height as f64 / max_sample / 2_f64;
+                    let x = (image_width as usize * n / sample_len) as i32;
+                    let y = (image_height as f64 / 2_f64 - half_height) as i32;
+                    let h = (half_height * 2_f64) as u32;
                     let h = if h <= 0 { 1 } else { h };
 
                     drawing::draw_hollow_rect_mut(&mut image,
-                                                  rect::Rect::at(x, y).of_size(w, h),
-                                                  white);
+                                                  rect::Rect::at(x, y).of_size(1_u32, h),
+                                                  foreground);
 
                 }
 
-                match image.save(format!("{}.png", path)) {
-                    Ok(_) => println!("done: {}", path),
-                    Err(e) => println!("err: {:?}", e),
-                };
-
+                let result = image.save(format!("{}.png", path));
+                if matches.is_present("verbose") {
+                    match result {
+                        Ok(_) => {
+                            terminal.fg(term::color::YELLOW).unwrap();
+                            print!("Done ");
+                            terminal.reset().unwrap();
+                            terminal.fg(term::color::GREEN).unwrap();
+                            print!("{}/{}", count, total_pathes);
+                            terminal.reset().unwrap();
+                            println!(" \"{}\"", path);
+                        }
+                        Err(e) => {
+                            terminal.fg(term::color::RED).unwrap();
+                            print!("Err ");
+                            terminal.reset().unwrap();
+                            terminal.fg(term::color::GREEN).unwrap();
+                            print!("{}/{}", count, total_pathes);
+                            terminal.reset().unwrap();
+                            println!("{}(\"{}\")", e.description(), path);
+                        }
+                    }
+                }
             }
 
-            Err(e) => println!("Cannot open file {}", e),
+            Err(e) => {
+                terminal.fg(term::color::RED).unwrap();
+                print!("Cannot open ");
+                terminal.fg(term::color::GREEN).unwrap();
+                print!("{}/{} ", count, total_pathes);
+                terminal.reset().unwrap();
+                println!("{}(\"{}\")", e.description(), path);
+            }
         }
     }
 
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use std::f64;
+    use std::i64;
+
+    #[test]
+    fn max() {
+        let vector = vec![0f64, 1f64, 2f64];
+        let max = vector.iter().cloned().fold(f64::MIN, f64::max);
+        assert_eq!(max, 2f64);
+    }
+
+    #[test]
+    fn hex_to_num() {
+        assert_eq!(i64::from_str_radix("ff", 16).unwrap(), 255);
+        assert_eq!(i64::from_str_radix("00", 16).unwrap(), 0);
+        assert_eq!(i64::from_str_radix("01", 16).unwrap(), 1);
+    }
 }
