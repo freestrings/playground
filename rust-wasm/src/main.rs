f463581942f355c92b0e58031f4cc8f0fb868bb9
freestrings <freestrings@gmail.com>
2017-06-16 11:35:32 +0900

    [rust-wasm] 리팩토링

--- rust-wasm/src/main.rs
+++ rust-wasm/src/main.rs
@@ -49,26 +49,23 @@ const BLOCK_Z: &[(u8, u8)] = &[(0, 0), (1, 0), (1, 1), (2, 1)];
 // #, #
 const BLOCK_O: &[(u8, u8)] = &[(0, 0), (1, 0), (0, 1), (1, 1)];
 
-
 fn main() {
     let sdl_context = sdl2::init().unwrap();
-
     let events = sdl_context.event_pump().unwrap();
-
     let video_subsystem = sdl_context.video().unwrap();
-    let window = video_subsystem.window("Test", COLUMNS * SCALE, ROWS * SCALE)
+    let window = video_subsystem
+        .window("Test", COLUMNS * SCALE, ROWS * SCALE)
         .build()
         .unwrap();
-    let canvas: WindowCanvas = window.into_canvas()
+    let canvas: WindowCanvas = window
+        .into_canvas()
         .accelerated()
         .target_texture()
         .build()
         .unwrap();
 
     let texture_creator: TextureCreator<WindowContext> = canvas.texture_creator();
-
     let mut app = Box::new(App::new(canvas, events, &texture_creator));
-
     let app_ptr = &mut *app as *mut App as *mut c_void;
 
     unsafe {
@@ -76,6 +73,13 @@ fn main() {
     }
 
     mem::forget(app);
+}
+
+extern "C" fn main_loop_callback(arg: *mut c_void) {
+    unsafe {
+        let mut app: &mut App = mem::transmute(arg);
+        app.draw();
+    }
 }
 
 struct KeyHandler {
@@ -162,52 +166,51 @@ impl Gravity {
         if self.counter > self.amount as i32 {
             self.counter = 0;
 
-            Some(points.iter()
-                .map(|point| Point::new(point.x(), point.y() + 1))
-                .collect())
+            Some(
+                points
+                    .iter()
+                    .map(|point| Point::new(point.x(), point.y() + 1))
+                    .collect(),
+            )
         } else {
             None
         }
     }
 }
 
-struct App<'a> {
-    canvas: Canvas<Window>,
-    events: EventPump,
-    texture: Texture<'a>,
-    current_block: Vec<Point>,
-    key_handler: KeyHandler,
-    gravity: Gravity,
+struct BlockHandler {
+    points: Vec<Point>,
 }
 
-impl<'a> App<'a> {
-    fn new(canvas: WindowCanvas,
-           events: EventPump,
-           texture_creator: &'a TextureCreator<WindowContext>)
-           -> App {
+impl BlockHandler {
+    fn new(points: Vec<Point>) -> BlockHandler {
+        BlockHandler { points: points }
+    }
 
-        let texture = texture_creator.create_texture_target(None, COLUMNS, ROWS)
-            .unwrap();
+    fn get_points(&mut self) -> Vec<Point> {
+        self.points.clone()
+    }
 
-        App {
-            canvas: canvas,
-            events: events,
-            texture: texture,
-            current_block: BLOCK_Z.iter()
-                .map(|raw_point| Point::new(raw_point.0 as i32, raw_point.1 as i32))
-                .collect(),
-            key_handler: KeyHandler::new(),
-            gravity: Gravity::new(DEFAULT_GRAVITY),
+    fn handle(&mut self, keycode: Keycode) {
+        match keycode {
+            Keycode::Up => self.rotate(),
+            Keycode::Left => self.move_left(),
+            Keycode::Right => self.move_right(),
+            Keycode::Down => self.move_down(),
+            _ => (),
         }
     }
 
     //
     // https://www.youtube.com/watch?v=Atlr5vvdchY
     //
-    fn block_rotate(points: &Vec<Point>, center: Point) -> Vec<Point> {
+    fn rotate(&mut self) {
         let angle = PI * 1.5_f32;
 
-        points.iter()
+        let center = self.center();
+
+        let mut points = self.points
+            .iter()
             .map(|point| {
                 let x = point.x() - center.x();
                 let y = point.y() - center.y();
@@ -220,39 +223,50 @@ impl<'a> App<'a> {
 
                 Point::new(rotated_x, rotated_y)
             })
-            .collect()
+            .collect();
 
+        self.replace(&mut points);
     }
 
-    fn block_move<F>(points: &Vec<Point>, mut f: F) -> Vec<Point>
-        where F: FnMut(&Point) -> Point
+    fn shift<F>(&mut self, mut f: F)
+    where
+        F: FnMut() -> (i32, i32),
     {
-        points.iter().map(|point| f(point)).collect()
+        let mut points = self.points
+            .iter()
+            .map(|point| {
+                let raw_point = f();
+                Point::new(point.x() + raw_point.0, point.y() + raw_point.1)
+            })
+            .collect();
+
+        self.replace(&mut points);
     }
 
-    fn block_move_left(points: &Vec<Point>) -> Vec<Point> {
-        Self::block_move(points, |point| Point::new(point.x - 1, point.y))
+    fn move_left(&mut self) {
+        self.shift(|| (-1, 0));
     }
 
-    fn block_move_right(points: &Vec<Point>) -> Vec<Point> {
-        Self::block_move(points, |point| Point::new(point.x + 1, point.y))
+    fn move_right(&mut self) {
+        self.shift(|| (1, 0));
     }
 
-    fn block_move_down(points: &Vec<Point>) -> Vec<Point> {
-        Self::block_move(points, |point| Point::new(point.x, point.y + 1))
+    fn move_down(&mut self) {
+        self.shift(|| (0, 1));
     }
 
-    fn block_center(points: &Vec<Point>) -> Point {
-        Point::new(points[2].x(), points[2].y())
+    fn center(&mut self) -> Point {
+        Point::new(self.points[2].x(), self.points[2].y())
     }
 
-    fn block_range(points: &Vec<Point>) -> Rect {
+    fn range(&mut self) -> Rect {
 
         let mut min_x = i32::max_value();
         let mut max_x = i32::min_value();
         let mut min_y = i32::max_value();
         let mut max_y = i32::min_value();
 
+        let points = &self.points;
         for b in points {
             if b.x.gt(&max_x) {
                 max_x = b.x;
@@ -268,13 +282,56 @@ impl<'a> App<'a> {
             }
         }
 
-        Rect::new(min_x,
-                  min_y,
-                  (max_x - min_x).abs() as u32,
-                  (max_y - min_y).abs() as u32)
+        Rect::new(
+            min_x,
+            min_y,
+            (max_x - min_x).abs() as u32,
+            (max_y - min_y).abs() as u32,
+        )
     }
 
-    fn apply_gravity(&mut self) {
+    fn replace(&mut self, target_points: &mut Vec<Point>) {
+        self.points.truncate(0);
+        self.points.append(target_points);
+    }
+}
+
+struct App<'a> {
+    canvas: Canvas<Window>,
+    events: EventPump,
+    texture: Texture<'a>,
+    current_block: Vec<Point>,
+    key_handler: KeyHandler,
+    gravity: Gravity,
+}
+
+impl<'a> App<'a> {
+    fn new(
+        canvas: WindowCanvas,
+        events: EventPump,
+        texture_creator: &'a TextureCreator<WindowContext>,
+    ) -> App {
+
+        let texture = texture_creator
+            .create_texture_target(None, COLUMNS, ROWS)
+            .unwrap();
+
+        App {
+            canvas: canvas,
+            events: events,
+            texture: texture,
+            current_block: BLOCK_Z
+                .iter()
+                .map(|raw_point| {
+                    Point::new(raw_point.0 as i32, raw_point.1 as i32)
+                })
+                .collect(),
+            key_handler: KeyHandler::new(),
+            gravity: Gravity::new(DEFAULT_GRAVITY),
+        }
+    }
+
+    fn apply_gravity_to_current_block(&mut self) {
         let applied = self.gravity.applied(&self.current_block);
         if let Some(mut points) = applied {
             self.current_block.truncate(0);
@@ -284,82 +341,74 @@ impl<'a> App<'a> {
 
     fn block_move_by_keyevent(&mut self) {
         let events = &mut self.events;
-        let block_rotator = &mut self.key_handler;
+        let key_handler = &mut self.key_handler;
 
-        for key in block_rotator.get_keycodes(events) {
-            let mut points = match key {
-                Keycode::Up => {
-                    Self::block_rotate(&self.current_block, Self::block_center(&self.current_block))
-                }
-                Keycode::Down => Self::block_move_down(&self.current_block),
-                Keycode::Left => Self::block_move_left(&self.current_block),
-                Keycode::Right => Self::block_move_right(&self.current_block),
-                Keycode::Space => {
-                    self.current_block.clone()
-                }
-                _ => self.current_block.clone(),
-            };
+        let mut block_handler = BlockHandler::new(self.current_block.clone());
+        for key in key_handler.get_keycodes(events) {
+            block_handler.handle(key);
 
-            let range = Self::block_range(&points);
+            let range = block_handler.range();
 
             if range.x() < 0 {
                 for _ in 0..range.x().abs() {
-                    let mut adjusted = Self::block_move_right(&points);
-                    points.truncate(0);
-                    points.append(&mut adjusted);
+                    block_handler.move_right();
                 }
             }
 
             let right = range.x() + range.width() as i32;
             if right >= COLUMNS as i32 {
                 for _ in 0..(right - COLUMNS as i32) + 1 {
-                    let mut adjusted = Self::block_move_left(&points);
-                    points.truncate(0);
-                    points.append(&mut adjusted);
+                    block_handler.move_left();
                 }
             }
-
-            self.current_block.truncate(0);
-            self.current_block.append(&mut points);
         }
+
+        self.current_block.truncate(0);
+        self.current_block.append(&mut block_handler.get_points());
     }
 
-    fn draw(&mut self) {
-        self.apply_gravity();
-        self.block_move_by_keyevent();
+    fn draw_background(&mut self) {
+        self.canvas.set_draw_color(Color::RGB(0, 0, 0));
+        self.canvas.clear();
+    }
 
+    fn present(&mut self) {
+        self.canvas.present();
+    }
+
+    fn draw_current_block(&mut self) {
         let canvas = &mut self.canvas;
         let points = &mut self.current_block;
         let texture = &mut self.texture;
 
-        canvas.set_draw_color(Color::RGB(0, 0, 0));
-        canvas.clear();
-
         for point in points.iter() {
             let src = Some(Rect::new(point.x(), point.y(), 1, 1));
-            let dst = Some(Rect::new(point.x() * SCALE as i32 + 1,
-                                     point.y() * SCALE as i32 + 1,
-                                     SCALE - 2,
-                                     SCALE - 2));
+            let dst = Some(Rect::new(
+                point.x() * SCALE as i32 + 1,
+                point.y() * SCALE as i32 + 1,
+                SCALE - 2,
+                SCALE - 2,
+            ));
 
-            canvas.with_texture_canvas(texture, |texture_canvas| {
+            canvas
+                .with_texture_canvas(texture, |texture_canvas| {
                     texture_canvas.clear();
                     texture_canvas.set_draw_color(Color::RGB(255, 0, 0));
-                    texture_canvas.draw_point(Point::new(point.x(), point.y()))
+                    texture_canvas
+                        .draw_point(Point::new(point.x(), point.y()))
                         .unwrap();
                 })
                 .unwrap();
 
             canvas.copy(&texture, src, dst).unwrap();
         }
-
-        canvas.present();
     }
-}
 
-extern "C" fn main_loop_callback(arg: *mut c_void) {
-    unsafe {
-        let mut app: &mut App = mem::transmute(arg);
-        app.draw();
+    fn draw(&mut self) {
+        self.apply_gravity_to_current_block();
+        self.block_move_by_keyevent();
+        self.draw_background();
+        self.draw_current_block();
+        self.present();
     }
 }
