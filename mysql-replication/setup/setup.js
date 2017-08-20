4537e7c484195b7c76d33cc4284e4531f13967c5
freestrings <freestrings@gmail.com>
2017-08-20 22:50:46 +0900

    [mysql-replication] docker, slave-of-slave

--- mysql-replication/setup/setup.js
+++ mysql-replication/setup/setup.js
@@ -12,19 +12,38 @@ const SLAVE = {
     user: 'root',
     password: 'root'
 };
+const SLAVE_OF_SLAVE = {
+    host: 'localhost',
+    port: '3308',
+    user: 'root',
+    password: 'root'
+};
 
-if (runAndCheck('./master-config') === true && runAndCheck('./slave-config') === true) {
+if (runAndCheck('./master-config') === true && runAndCheck('./slave-config') === true && runAndCheck('./slave-of-slave-config') === true) {
     grantReplication()
         .then(getMasterInfo)
         .then(configureSlave)
         .then(startSlave)
         .then(getSlaveInfo)
+        .then(grantToSlaveOfSlaveReplication)
+        .then(getMasterInfoOfSlave)
+        .then(configureSlaveOfSlave)
+        .then(startSlaveOfSlave)
+        .then(getSlaveOfSlaveInfo)
         .then(function() { console.log('Done!') })
         .catch(console.error);
+} else {
+    console.error('Fail to configuration');
 }
 
 function runAndCheck(command) {
-    return JSON.parse(sync('bash', [command]).stdout.toString())[0].State.Running;
+    var out = sync('bash', [command]).stdout.toString();
+    try {
+        return JSON.parse(out)[0].State.Running;
+    } catch (e) {
+        console.error(command, e, '[', out, ']');
+        return false;
+    }
 }
 
 function query(retry, info, sql, callback) {
@@ -85,7 +104,7 @@ function getMasterInfo() {
 }
 
 function configureSlave(masterInfo) {
-    process.stdout.write('configureSlave');
+    process.stdout.write('configureSlave:' + masterInfo.File + ',' + masterInfo.Position);
 
     return new Promise(function(resolve, reject) {
         query(10,
@@ -131,6 +150,102 @@ function getSlaveInfo() {
     return new Promise(function(resolve, reject) {
         query(10,
             SLAVE,
+            'SHOW SLAVE STATUS',
+            function(results) {
+                if (results.error) {
+                    reject(result.message);
+                } else {
+                    console.log(results[0]);
+                    resolve();
+                }
+            }
+        )
+    });
+}
+
+function grantToSlaveOfSlaveReplication() {
+    process.stdout.write('grantToSlaveOfSlaveReplication');
+
+    return new Promise(function(resolve, reject) {
+        query(10,
+            SLAVE,
+            "GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%' IDENTIFIED BY 'slavepass'",
+            function(results) {
+                if (results.error) {
+                    reject(results.message);
+                } else {
+                    resolve();
+                }
+            }
+        )
+    });
+}
+
+function getMasterInfoOfSlave() {
+    process.stdout.write('getMasterInfoOfSlave');
+
+    return new Promise(function(resolve, reject) {
+        query(10,
+            SLAVE,
+            'SHOW MASTER STATUS',
+            function(results) {
+                if (results.error) {
+                    reject(results.message);
+                } else {
+                    resolve(results[0]);
+                }
+            }
+        )
+    });
+}
+
+function configureSlaveOfSlave(masterInfo) {
+    process.stdout.write('configureSlaveOfSlave:' + masterInfo.File + ',' + masterInfo.Position);
+
+    return new Promise(function(resolve, reject) {
+        query(10,
+            SLAVE_OF_SLAVE, [
+                'CHANGE MASTER TO MASTER_HOST="mysql-slave"',
+                'MASTER_USER="repl"',
+                'MASTER_PASSWORD="slavepass"',
+                'MASTER_LOG_FILE="' + masterInfo.File + '"',
+                'MASTER_LOG_POS=' + masterInfo.Position
+            ].join(','),
+            function(results) {
+                if (results.error) {
+                    reject(result.message);
+                } else {
+                    resolve();
+                }
+            }
+        )
+    });
+}
+
+function startSlaveOfSlave() {
+    process.stdout.write('startSlaveOfSlave');
+
+    return new Promise(function(resolve, reject) {
+        query(10,
+            SLAVE_OF_SLAVE,
+            'START SLAVE',
+            function(results) {
+                if (results.error) {
+                    reject(result.message);
+                } else {
+                    resolve();
+                }
+            }
+        )
+    });
+}
+
+function getSlaveOfSlaveInfo() {
+    process.stdout.write('getSlaveOfSlaveInfo');
+
+    return new Promise(function(resolve, reject) {
+        query(10,
+            SLAVE_OF_SLAVE,
             'SHOW SLAVE STATUS',
             function(results) {
                 if (results.error) {
