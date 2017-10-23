e620d40d505ce3d1d7c9c45b4811bbf1e9689450
freestrings <freestrings@gmail.com>
2017-10-24 00:28:36 +0900

    [rust-tokio] named types

--- rust-tokio/src/future_test.rs
+++ rust-tokio/src/future_test.rs
@@ -3,6 +3,9 @@ extern crate tokio_timer;
 
 const BIG_PRIME: u64 = 15485867;
 
+use futures::Future;
+use futures::Map;
+
 fn is_prime(num: u64) -> bool {
 
     for i in 2..num {
@@ -14,6 +17,21 @@ fn is_prime(num: u64) -> bool {
     true
 }
 
+//
+// not yet stable! "impl Trait"
+//
+// fn add_10<F>(f: F) -> impl Future<Item = i32, Error = F::Error>
+//     where F: Future<Item = i32>,
+// {
+//     f.map(|i| i + 10)
+// }
+
+fn add_10<F>(f: F) -> Map<F, fn(i32) -> i32>
+    where F: Future<Item = i32>,
+{
+    fn do_map(i: i32) -> i32 { i + 10 }
+    f.map(do_map)
+}
 
 #[cfg(test)]
 mod tests {
@@ -22,6 +40,8 @@ mod tests {
 
     use std::time::Duration;
 
+    use futures::prelude::*;
+    use futures::future;
     use futures::Future;
     use super::futures_cpupool::CpuPool;
     use super::tokio_timer::Timer;
@@ -52,6 +72,28 @@ mod tests {
             Ok(true) => println!("Prime"),
             Ok(false) => println!("Not prime"),
             Err(_) => println!("Timed out"),
+        }
+    }
+
+    #[test]
+    fn fuse() {
+        let mut future = future::ok::<i32, u32>(2);
+        assert_eq!(future.poll(), Ok(Async::Ready(2)));
+        //assert_eq!(future.poll(), Ok(Async::Ready(2))); => error
+
+        let mut future = future::ok::<i32, u32>(2).fuse();
+        assert_eq!(future.poll(), Ok(Async::Ready(2)));
+        assert_eq!(future.poll(), Ok(Async::NotReady));
+        assert_eq!(future.poll(), Ok(Async::NotReady));
+        assert_eq!(future.poll(), Ok(Async::NotReady));
+    }
+
+    #[test]
+    fn add_10() {
+        let mut future = future::ok::<i32, u32>(2);
+        match super::add_10(future).poll() {
+            Ok(Async::Ready(x)) => assert_eq!(12, x),
+            _ => panic!("woops"),
         }
     }
 }
