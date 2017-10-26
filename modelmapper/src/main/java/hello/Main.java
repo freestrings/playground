cc559b7ba3bd5182edd2da7674f7be312b491606
freestrings <freestrings@gmail.com>
2017-10-26 18:56:50 +0900

    [modelmapper] groupping

--- modelmapper/src/main/java/hello/Main.java
+++ modelmapper/src/main/java/hello/Main.java
@@ -1,7 +1,9 @@
 package hello;
 
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -31,5 +33,31 @@ public class Main {
 
         System.out.println(orderDto2.getOrderId());
         System.out.println(orderDto2.getOrderName());
+
+        List<Order> items = Arrays.asList(
+                Order.builder().orderId(1).orderName("name1").address("address1").build(),
+                Order.builder().orderId(1).orderName("name2").address("address2").build(),
+                Order.builder().orderId(1).orderName("name2").address("address2-2").build(),
+                Order.builder().orderId(1).orderName("name3").address("address1").build(),
+                Order.builder().orderId(1).orderName("name4").address("address2").build(),
+                Order.builder().orderId(2).orderName("name1").address("address").build()
+        );
+
+        Map<Integer, Map<String, String>> collect = items.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                order -> order.getOrderId(),
+                                Collectors.mapping(
+                                        order -> order,
+                                        Collectors.toMap(
+                                                order -> order.getOrderName(),
+                                                order -> order.getAddress(),
+                                                (k1, k2) -> k1,
+                                                LinkedHashMap::new
+                                        )
+                                )
+                        )
+                );
+        System.out.println(collect);
     }
 }
--- modelmapper/src/main/java/hello/Order.java
+++ modelmapper/src/main/java/hello/Order.java
@@ -2,12 +2,16 @@ package hello;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Builder
 @Getter
+@ToString
 public class Order {
 
     private Integer orderId;
 
     private String orderName;
+
+    private String address;
 }
