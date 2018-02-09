f73383d0de5f834d5fb8769f42fe59bc338c252a
freestrings <freestrings@gmail.com>
2018-02-09 14:35:09 +0900

    [java-classloader] set the log4j2.xml location promatically

--- java-classloader/fs/playground/App.java
+++ java-classloader/fs/playground/App.java
@@ -1,6 +1,8 @@
 package fs.playground;
 
+import java.io.File;
 import java.lang.reflect.Method;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Arrays;
@@ -13,7 +15,7 @@ public class App {
 
     public static void main(String... args) throws Exception {
         App app = new App();
-        app.log4j1();
+//        app.log4j1();
         app.log4j2();
     }
 
@@ -25,15 +27,16 @@ public class App {
         debug.invoke(logger, "log4j1");
     }
 
-    /**
-     * -Dlog4j2.debug=true
-     * -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector
-     * @throws Exception
-     */
     private void log4j2() throws Exception {
         URLClassLoader loader = getLoader("../../lib/log4j-api-2.10.0.jar", "../../lib/log4j-core-2.10.0.jar", "../../lib/disruptor-3.3.7.jar");
-        Class<?> aClass = Class.forName("org.apache.logging.log4j.LogManager", false, loader);
-        Object logger = aClass.getDeclaredMethod("getLogger", Class.class).invoke(null, App.class);
+        Class<?> logManagerClass = Class.forName("org.apache.logging.log4j.LogManager", false, loader);
+
+        Method getContext = logManagerClass.getDeclaredMethod("getContext", boolean.class);
+        Object context = getContext.invoke(null, false);
+        Method setConfigLocation = context.getClass().getMethod("setConfigLocation", URI.class);
+        setConfigLocation.invoke(context, new File("./lib/log4j2.xml").toURI());
+
+        Object logger = logManagerClass.getDeclaredMethod("getLogger", Class.class).invoke(null, App.class);
         Method info = logger.getClass().getMethod("info", String.class);
         info.invoke(logger, "log4j2");
     }
