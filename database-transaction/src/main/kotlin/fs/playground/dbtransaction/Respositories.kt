4eeab62248929bc0f66e615648fb9f4ea2407e5a
freestrings <freestrings@gmail.com>
2018-07-11 23:23:03 +0900

    [database-transaction] setLockMode(LockModeType.PESSIMISTIC_WRITE) 실제 쿼리 확인

--- database-transaction/src/main/kotlin/fs/playground/dbtransaction/Respositories.kt
+++ database-transaction/src/main/kotlin/fs/playground/dbtransaction/Respositories.kt
@@ -20,6 +20,18 @@ class TestTableRepositoryImpl(
         @Autowired val dataSource: DataSource
 ) : TestTableRepositoryExt {
 
+    /**
+     * setLockMode(LockModeType.PESSIMISTIC_WRITE) 하면 실제 select ... for update 구문이 생성된다.
+     *
+     * cat /var/log/mysql/query.log | grep "2 Query"
+     * ...
+     * 2018-07-11T14:17:05.522767Z	    2 Query	SET autocommit=1
+     * 2018-07-11T14:17:05.539209Z	    2 Query	SET autocommit=0
+     * 2018-07-11T14:17:05.756364Z	    2 Query	select testtable0_.id as id1_0_, testtable0_.count as count2_0_, testtable0_.count_limit as count_li3_0_ from test_table testtable0_ where testtable0_.id='testa' for update
+     * 2018-07-11T14:17:05.790807Z	    2 Query	update test_table set count=5, count_limit=10 where id='testa'
+     * 2018-07-11T14:17:05.791537Z	    2 Query	commit
+     * ...
+     */
     override fun increase(id: String) {
         val query = entityManager.createQuery("select t from TestTable t where id = :name", TestTable::class.java)
         query.setParameter("name", id)
