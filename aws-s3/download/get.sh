3473ad02f05050466111e1ccf7d12da2adb2a1e3
changseok han <freestrings@gmail.com>
2018-11-09 15:26:30 +0900

    [aws-s3] get.sh 압축풀기 추가

--- aws-s3/download/get.sh
+++ aws-s3/download/get.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 docker run -it --rm -v `pwd`/downloaded:/downloaded freestrings/aws-s3-download node /download.js $1 $2 "directcs-docker-images-test" $3
+sudo chown -R ubuntu:ubuntu ./downloaded
+tar -xvzf "./downloaded/${3}" -C ./downloaded
