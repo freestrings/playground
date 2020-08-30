e28121fa1a0d9e47208713db575bfaeddd1e4224
freestrings <freestrings@gmail.com>
2020-08-30 18:55:49 +0900

    jsql-parser : check column - init

--- jsql-parser/src/main/kotlin/fs/playground/App.kt
+++ jsql-parser/src/main/kotlin/fs/playground/App.kt
@@ -1,89 +1,230 @@
 package fs.playground
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import net.sf.jsqlparser.expression.*
 import net.sf.jsqlparser.expression.Function
 import net.sf.jsqlparser.expression.operators.relational.*
 import net.sf.jsqlparser.parser.CCJSqlParserUtil
+import net.sf.jsqlparser.schema.Column
 import net.sf.jsqlparser.schema.Table
 import net.sf.jsqlparser.statement.select.*
 
 fun main(args: Array<String>) {
-    val sql = getSql9()
+    val tableMeta = loadTableMeta()
+    val sql = getSql1_2()
     println("input: $sql")
     val select = CCJSqlParserUtil.parse(sql) as Select
-    select.selectBody.accept(SimpleSelectVisitor())
+    val parseEventEmitter = ParseEventEmitter()
+    parseEventEmitter.addLister(DefaultParseEventListener(tableMeta))
+    select.selectBody.accept(SimpleSelectVisitor(parseEventEmitter))
 
 }
 
-class SimpleFromItemVisitor : FromItemVisitorAdapter() {
-    override fun visit(table: Table?) {
-        println("table: ${table?.let { it.name }} - ${table?.let { it.alias }}")
-        super.visit(table)
-    }
-
-    override fun visit(subSelect: SubSelect?) {
-        println("subselect")
-        subSelect?.let { it.selectBody.accept(SimpleSelectVisitor()) }
-    }
-
-    override fun visit(subjoin: SubJoin?) {
-        println("subjoin")
-        super.visit(subjoin)
-    }
-
-    override fun visit(lateralSubSelect: LateralSubSelect?) {
-        println("lateral subselect")
-        super.visit(lateralSubSelect)
-    }
-
-    override fun visit(valuesList: ValuesList?) {
-        println("value list")
-        super.visit(valuesList)
-    }
+fun loadTableMeta(): Map<String, Any> {
+    val resource = Thread.currentThread().contextClassLoader.getResource("table.json")
+    val mapper = jacksonObjectMapper()
+    val typeRef = object : TypeReference<Map<String, Any>>() {}
+    return mapper.readValue(resource.openStream(), typeRef)
 }
 
-class SimpleSelectVisitor : SelectVisitorAdapter() {
-    override fun visit(plainSelect: PlainSelect?) {
-        println("plain select")
-        plainSelect?.let { plainSelect ->
-            plainSelect.selectItems.forEach { it.accept(SimpleSelectItemVisitor()) }
-            plainSelect.fromItem.accept(SimpleFromItemVisitor())
-            plainSelect.where?.let { it.accept(SimpleExpressionVisitor()) }
-            plainSelect.joins?.let {
-                it.forEach { join ->
-                    join.rightItem.accept(SimpleFromItemVisitor())
+fun isColumnExist(tableName: String, columnName: String, tablesMeta: Map<String, Any>): Boolean {
+    if (columnName == "*") {
+        return true
+    }
+
+    val tableMeta = tablesMeta[tableName.toLowerCase()] as Map<String, Any>?
+    return tableMeta?.let { table ->
+        val columns = table["columns"] as List<Map<String, String>>
+        columns.any { it["name"] == columnName.toLowerCase() }
+    } ?: false
+}
+
+class DefaultParseEventListener(private val tablesMeta: Map<String, Any>) : ParseEventListener {
+
+    enum class Status(i: Int) {
+        INIT(0),
+        ON_TABLE(1),
+        ON_COLUMN(2)
+    }
+
+    private val tables = mutableListOf<Map<String, String>>()
+    private var status = Status.INIT
+
+    override fun onEvent(e: ParseEvent) {
+        when (e.event) {
+            ParseEvent.Type.COLUMN -> {
+                println("-- column: ${e.data["columnName"]}, tableOrTableAlias: ${e.data["tableOrTableAlias"]}")
+                status = Status.ON_COLUMN
+
+                val columnName = e.data["columnName"] as String
+                val tableOrTableAliasOfColumn = e.data["tableOrTableAlias"]
+
+                tables
+                    .map { table ->
+                        val tableName = table["name"] as String
+                        val tableAlias = table["alias"]
+
+                        val tableCandidate = tableOrTableAliasOfColumn?.let { tableOfColumns ->
+                            when {
+                                tableAlias == tableOfColumns -> {
+                                    tableName
+                                }
+                                tableName == tableOfColumns -> {
+                                    tableName
+                                }
+                                else -> {
+                                    null
+                                }
+                            }
+                        } ?: tableName
+
+                        if (isColumnExist(tableCandidate, columnName, tablesMeta)) {
+                            println("1. $tableCandidate - $columnName")
+                        } else {
+                            println("2. $tableCandidate - $columnName")
+                        }
+
+                    }
+            }
+            ParseEvent.Type.TABLE -> {
+                println("-- table: ${e.data["name"]}, alias: ${e.data["alias"]}")
+
+                if (status == Status.ON_COLUMN) {
+                    tables.clear()
                 }
+
+                status = Status.ON_TABLE
+                tables.add(e.data)
+            }
+            ParseEvent.Type.BIND_VARIABLE -> {
+            }
+            ParseEvent.Type.LOG -> {
+                println("----$e")
             }
         }
     }
 
-    override fun visit(setOpList: SetOperationList?) {
-        println("setop list")
-        setOpList?.let { it.plainSelects.forEach(this::visit) }
-    }
+}
 
-    override fun visit(withItem: WithItem?) {
-        println("not implementation")
+data class ParseEvent(val event: Type, val data: Map<String, String>) {
+    enum class Type {
+        COLUMN,
+        BIND_VARIABLE,
+        LOG,
+        TABLE,
+        SUB_SELECT_START,
+        SUB_SELECT_END
     }
 }
 
-class SimpleSelectItemVisitor : SelectItemVisitorAdapter() {
+interface ParseEventListener {
+
+    fun onEvent(e: ParseEvent)
+}
+
+class ParseEventEmitter {
+
+    private val listeners = mutableListOf<ParseEventListener>()
+
+    fun addLister(listener: ParseEventListener) {
+        listeners.add(listener)
+    }
+
+    fun emit(e: ParseEvent) {
+        listeners.forEach { it.onEvent(e) }
+    }
+}
+
+class SimpleFromItemVisitor(private val parseEventEmitter: ParseEventEmitter) : FromItemVisitorAdapter() {
+    override fun visit(table: Table?) {
+        parseEventEmitter.emit(
+            ParseEvent(
+                ParseEvent.Type.TABLE,
+                table?.let { table ->
+                    mapOf(
+                        "name" to table.name,
+                        "alias" to (table.alias?.let { it.name } ?: "")
+                    )
+                } ?: mapOf()
+            )
+        )
+    }
+
+    override fun visit(subSelect: SubSelect?) {
+        parseEventEmitter.emit(ParseEvent(ParseEvent.Type.LOG, mapOf("trace" to "visit: subSelect: SubSelect?")))
+        subSelect?.let { it.selectBody.accept(SimpleSelectVisitor(parseEventEmitter)) }
+        parseEventEmitter.emit(ParseEvent(ParseEvent.Type.LOG, mapOf("trace" to "visit-end: subSelect: SubSelect?")))
+    }
+}
+
+class SimpleSelectVisitor(private val parseEventEmitter: ParseEventEmitter) : SelectVisitorAdapter() {
+
+    override fun visit(plainSelect: PlainSelect?) {
+        parseEventEmitter.emit(ParseEvent(ParseEvent.Type.LOG, mapOf("trace" to "visit: plainSelect: PlainSelect?")))
+
+        plainSelect?.let { plainSelect ->
+            val simpleSelectItemVisitor = SimpleSelectItemVisitor(parseEventEmitter)
+
+            plainSelect.fromItem.accept(SimpleFromItemVisitor(parseEventEmitter))
+            plainSelect.joins?.let { joins ->
+                joins.forEach {
+                    it.rightItem.accept(SimpleFromItemVisitor(parseEventEmitter))
+                }
+            }
+            plainSelect.distinct?.let { distinct ->
+                distinct.onSelectItems.forEach { it.accept(simpleSelectItemVisitor) }
+            }
+            plainSelect.selectItems?.let { selectItems ->
+                selectItems.forEach { it.accept(simpleSelectItemVisitor) }
+            }
+            plainSelect.where?.let { it.accept(SimpleExpressionVisitor(parseEventEmitter)) }
+        }
+
+        parseEventEmitter.emit(
+            ParseEvent(
+                ParseEvent.Type.LOG,
+                mapOf("trace" to "visit-end: plainSelect: PlainSelect?")
+            )
+        )
+    }
+
+    override fun visit(setOpList: SetOperationList?) {
+        parseEventEmitter.emit(ParseEvent(ParseEvent.Type.LOG, mapOf("trace" to "visit: setOpList: SetOperationList?")))
+        setOpList?.let { it.plainSelects.forEach(this::visit) }
+    }
+}
+
+class SimpleSelectItemVisitor(private val parseEventEmitter: ParseEventEmitter) : SelectItemVisitorAdapter() {
 
     override fun visit(columns: AllTableColumns?) {
         columns?.let {
-            val table = it.table
-            println("table: ${table?.let { it.name }} - ${table?.let { it.alias }}")
+            parseEventEmitter.emit(
+                ParseEvent(
+                    ParseEvent.Type.COLUMN,
+                    mapOf("columnName" to "*", "tableOrTableAlias" to it.table.name)
+                )
+            )
         }
     }
 
     override fun visit(item: SelectExpressionItem?) {
         item?.let {
-            it.expression.accept(SimpleExpressionVisitor())
+            it.expression.accept(SimpleExpressionVisitor(parseEventEmitter))
         }
+    }
+
+    override fun visit(columns: AllColumns?) {
+        parseEventEmitter.emit(
+            ParseEvent(
+                ParseEvent.Type.COLUMN,
+                mapOf("columnName" to "*")
+            )
+        )
     }
 }
 
-class SimpleExpressionVisitor : ExpressionVisitorAdapter() {
+class SimpleExpressionVisitor(private val parseEventEmitter: ParseEventEmitter) : ExpressionVisitorAdapter() {
 
     override fun visit(function: Function?) {
         function?.let { function ->
@@ -97,12 +238,13 @@ class SimpleExpressionVisitor : ExpressionVisitorAdapter() {
         }
     }
 
-    override fun visit(parameter: JdbcParameter?) {
-        super.visit(parameter)
-    }
-
     override fun visit(parameter: JdbcNamedParameter?) {
-        super.visit(parameter)
+        parseEventEmitter.emit(
+            ParseEvent(
+                ParseEvent.Type.BIND_VARIABLE,
+                mapOf("name" to parameter!!.name)
+            )
+        )
     }
 
     override fun visit(expr: EqualsTo?) {
@@ -144,7 +286,7 @@ class SimpleExpressionVisitor : ExpressionVisitorAdapter() {
     }
 
     override fun visit(subSelect: SubSelect?) {
-        subSelect?.let { it.accept(SimpleFromItemVisitor()) }
+        subSelect?.let { it.accept(SimpleFromItemVisitor(parseEventEmitter)) }
     }
 
     override fun visit(expr: CaseExpression?) {
@@ -164,11 +306,29 @@ class SimpleExpressionVisitor : ExpressionVisitorAdapter() {
     }
 
     override fun visit(expr: AllComparisonExpression?) {
-        expr?.let { it.subSelect.accept(SimpleFromItemVisitor()) }
+        expr?.let { it.subSelect.accept(SimpleFromItemVisitor(parseEventEmitter)) }
     }
 
     override fun visit(expr: AnyComparisonExpression?) {
-        expr?.let { it.subSelect.accept(SimpleFromItemVisitor()) }
+        expr?.let { it.subSelect.accept(SimpleFromItemVisitor(parseEventEmitter)) }
+    }
+
+    override fun visit(column: Column?) {
+        parseEventEmitter.emit(
+            ParseEvent(
+                ParseEvent.Type.COLUMN,
+                mapOf(
+                    "columnName" to column!!.columnName,
+                    "tableOrTableAlias" to if (column!!.fullyQualifiedName.indexOf('.') > -1) {
+                        column!!.fullyQualifiedName.substring(0, column!!.fullyQualifiedName.indexOf('.'))
+                    } else {
+                        ""
+                    },
+                    "fullyQualifiedName" to column!!.fullyQualifiedName,
+                    "inWhereCause" to "true"
+                )
+            )
+        )
     }
 }
 
@@ -185,7 +345,7 @@ fun getSql1(): String? {
     return """
         select * from (
             select 
-                * 
+                *
             from 
                 tab1 t1,
                 tab2 t2
@@ -197,11 +357,99 @@ fun getSql1(): String? {
             select 
                 * 
             from 
+                tab1 t3,
+                tab2 t4
+            where t3.a = t4.a
+                and t3.b = t4.b(+)
+        )
+    """.trimIndent()
+}
+
+fun getSql1_1(): String? {
+    return """
+        select t1.a, t2.a from (
+            select 
+                *
+            from 
                 tab1 t1,
                 tab2 t2
             where t1.a = t2.a
                 and t1.b = t2.b(+)
-        )
+                
+            union all
+            
+            select 
+                * 
+            from 
+                tab1 t3,
+                tab2 t4
+            where t3.a = t4.a
+                and t3.b = t4.b(+)
+        ) t1, 
+        (
+            select 
+                *
+            from 
+                tab1 k1,
+                tab2 k2
+            where k1.a = k2.a
+                and k1.b = k2.b(+)
+                
+            union all
+            
+            select 
+                * 
+            from 
+                tab1 k3,
+                tab2 k4
+            where k3.a = k4.a
+                and k3.b = k4.b(+)
+        ) t2
+        where t1.o = t2.o
+    """.trimIndent()
+}
+
+fun getSql1_2(): String? {
+    return """
+        select t1.a, t2.a from (
+            select 
+                t1.*
+            from 
+                tab1 t1,
+                tab2 t2
+            where t1.a = t2.a
+                and t1.b = t2.b(+)
+                
+            union all
+            
+            select 
+                t2.* 
+            from 
+                tab1 t3,
+                tab2 t4
+            where t3.a = t4.a
+                and t3.b = t4.b(+)
+        ) t1, 
+        (
+            select 
+                k1.a
+            from 
+                tab1 k1,
+                tab2 k2
+            where k1.a = k2.a
+                and k1.b = k2.b(+)
+                
+            union all
+            
+            select 
+                k4.b 
+            from 
+                tab1 k3,
+                tab2 k4
+            where k3.a = k4.a
+                and k3.b = k4.b(+)
+        ) t2
+        where t1.o = t2.o
     """.trimIndent()
 }
 
@@ -298,5 +546,20 @@ fun getSql9(): String? {
         select * from tab1 t1 
         where t1.a = cast(:a as unsinged)
             and t1.b = cast( (select a from t2) as unsigned )
+    """.trimIndent()
+}
+
+fun getSql10(): String? {
+    return """
+        select a, b, c as c1, t1.*, tab1.* from tab1 t1
+    """.trimIndent()
+}
+
+fun getSql11(): String? {
+    return """
+        select a, b, c as c1, t1.*, tab1.* from tab1 t1
+            inner join tab2 t2 on t1.a = t2.a
+                and t1.b = t2.b
+        where t1.z = (select i from tab3)
     """.trimIndent()
 }
