88a00ff19ff9ae8c416b6a9d48c67878d314147f
freestrings <freestrings@gmail.com>
2020-10-29 14:48:55 +0900

    LTCContext 적용

--- springframework-webflux-coroutine-jpa-transaction/src/main/kotlin/fs/playground/SpringframeworkWebfluxJpaTransactionApplication.kt
+++ springframework-webflux-coroutine-jpa-transaction/src/main/kotlin/fs/playground/SpringframeworkWebfluxJpaTransactionApplication.kt
@@ -1,13 +1,12 @@
 package fs.playground
 
 import com.zaxxer.hikari.HikariDataSource
-//import fs.playground.Dispatcher.asAsync
-//import fs.playground.Dispatcher.asReadonlyTransaction
+import fs.playground.LTCDispatcher.asAsync
+import fs.playground.LTCDispatcher.asReadonlyTransaction
 import fs.playground.entity.Person
 import fs.playground.repository.PersonRepository
 import kotlinx.coroutines.*
-import kotlinx.coroutines.slf4j.MDCContext
-import org.slf4j.MDC
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
@@ -28,11 +27,14 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.annotation.EnableTransactionManagement
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.transaction.support.DefaultTransactionStatus
 import org.springframework.transaction.support.TransactionSynchronizationManager
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 import javax.sql.DataSource
+import kotlin.coroutines.AbstractCoroutineContextElement
+import kotlin.coroutines.CoroutineContext
 
 @SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
 class SpringframeworkWebfluxJpaApplication
@@ -41,206 +43,264 @@ fun main(args: Array<String>) {
     runApplication<SpringframeworkWebfluxJpaApplication>(*args)
 }
 
-//@RestController
-//class Ctrl(val psersonService: PersonService) {
-//
-//    @GetMapping("/master/read")
-//    suspend fun readFromMaster(): Long {
-//        return psersonService.readFromMaster("master - ${UUID.randomUUID().toString()}")
-//    }
-//
-//    @GetMapping("/master/write")
-//    suspend fun writeToMaster() {
-//        psersonService.writeToMaster(UUID.randomUUID().toString())
-//    }
-//
-//    @GetMapping("/master/write/asreadonly")
-//    suspend fun writeToMasterAsReadonly() {
-//        psersonService.writeToMasterAsReadonly(UUID.randomUUID().toString())
-//    }
-//
-//    @GetMapping("/slave/read")
-//    suspend fun readFromSlave(): Long {
-//        return psersonService.readFromSlave("slave - ${UUID.randomUUID().toString()}")
-//    }
-//
-//    @GetMapping("/slave/write")
-//    suspend fun writeToSlaveAsReadonly() {
-//        psersonService.writeToSlaveAsReadonly(UUID.randomUUID().toString())
-//    }
-//
-//    @GetMapping("/slave/readall")
-//    suspend fun readAllFromSlave() {
-//        psersonService.readAllFromSlave("slaveall - ${UUID.randomUUID().toString()}")
-//    }
-//
-//    @GetMapping("/slave/read/async")
-//    suspend fun readFromSlaveAsyc(): Long {
-//        return psersonService.readFromSlaveAsync(UUID.randomUUID().toString())
-//    }
-//
-//}
-//
-//@Service
-//class PersonService(val personRepository: PersonRepository) {
-//
-//    suspend fun readFromMaster(uuid: String) = personRepository.countByName(uuid)
-//
-//    @Transactional
-//    suspend fun writeToMaster(uuid: String) = personRepository.save(Person(name = uuid))
-//
-//    suspend fun readAllFromSlave(uuid: String): Long {
-//        return asReadonlyTransaction {
-//            val c1 = asAsync { personRepository.countByName(uuid) }
-//            val c2 = asAsync { personRepository.countByName(uuid) }
-//            val c3 = asReadonlyTransaction {
-//                val c3 = asAsync { personRepository.countByName(uuid) }
-//                c3
-//            }
-//            val (r1, r2, r3) = awaitAll(c1, c2, c3)
-//            r1 + r2 + r3
-//        }
-//    }
-//
-//    /**
-//     * @Transactional 때문에 master에 write 된다.
-//     */
-//    @Transactional
-//    suspend fun writeToMasterAsReadonly(uuid: String) {
-//        return asReadonlyTransaction {
-//            personRepository.save(Person(name = uuid))
-//        }
-//    }
-//
-//    /**
-//     * Exception 발생
-//     */
-//    suspend fun writeToSlaveAsReadonly(uuid: String) {
-//        return asReadonlyTransaction {
-//            writeToMaster(uuid)
-//        }
-//    }
-//
-//    suspend fun readFromSlave(uuid: String): Long {
-//        return asReadonlyTransaction { personRepository.countByName(uuid) }
-//    }
-//
-//    suspend fun readFromSlaveAsync(uuid: String): Long {
-//        return asReadonlyTransaction {
-//            val c1 = asAsync { personRepository.countByName(uuid) }
-//            c1.await()
-//        }
-//    }
-//}
-//
-///**
-// * ThreadLocal 처리는 별도 구현하기 보다 안전하게 MDC 구현을 사용한다.
-// */
-//object Dispatcher {
-//    private val KEY_READONLY = UUID.randomUUID().toString()
-//
-//    fun <T> asAsync(call: () -> T): Deferred<T> = CoroutineScope(MDCContext() + Dispatchers.Default).async {
-//        call()
-//    }
-//
-//    suspend fun <T> asReadonlyTransaction(call: suspend () -> T): T {
-//        return withContext(MDCContext()) {
-//            if (!isCurrentTransactionReadOnly()) {
-//                /**
-//                 * 코로틴을 사용하는 코드에서 MDC에서 별도로 키를 remove 하려면 일반적으로 스레드가 다르기 때문에 주의가 필요하다.
-//                 * withContext를 사용하면 안전하다.
-//                 */
-//                MDC.put(KEY_READONLY, "evada")
-//                withContext(MDCContext()) {
-//                    call()
-//                }
-//            } else {
-//                call()
-//            }
-//        }
-//    }
-//
-//    fun isCurrentTransactionReadOnly() = MDC.get(KEY_READONLY) != null
-//}
-//
-//@Configuration
-//@EnableJpaRepositories(
-//        basePackages = ["fs.playground.repository"],
-//        enableDefaultTransactions = false
-//)
-//@EnableTransactionManagement
-//class Config {
-//
-//    companion object {
-//        val SLAVE_DB_KEY = "slave"
-//        val MASTER_DB_KEY = "master"
-//    }
-//
-//    @Bean(name = ["masterDataSource"])
-//    @ConfigurationProperties(prefix = "master.datasource")
-//    fun masterDataSource(): DataSource? {
-//        return DataSourceBuilder.create().type(HikariDataSource::class.java).build()
-//    }
-//
-//    @Bean(name = ["slaveDataSource"])
-//    @ConfigurationProperties(prefix = "slave.datasource")
-//    fun slaveDataSource(): DataSource? {
-//        return DataSourceBuilder.create().type(HikariDataSource::class.java).build()
-//    }
-//
-//    @Bean(name = ["routingDataSource"])
-//    fun routingDataSource(
-//            @Qualifier("masterDataSource") masterDataSource: DataSource,
-//            @Qualifier("slaveDataSource") slaveDataSource: DataSource,
-//    ): DataSource {
-//        val routingDataSource = ReplicationRoutingDataSource()
-//        val dataSourceMap: MutableMap<Any, Any> = HashMap()
-//        dataSourceMap[MASTER_DB_KEY] = masterDataSource
-//        dataSourceMap[SLAVE_DB_KEY] = slaveDataSource
-//        routingDataSource.setTargetDataSources(dataSourceMap)
-//        routingDataSource.setDefaultTargetDataSource(masterDataSource)
-//        return routingDataSource
-//    }
-//
-//    @Primary
-//    @Bean
-//    fun dataSource(@Qualifier("routingDataSource") routingDataSource: DataSource): DataSource? {
-//        return LazyConnectionDataSourceProxy(routingDataSource)
-//    }
-//
-//    @Bean
-//    fun entityManagerFactory(dataSource: DataSource): LocalContainerEntityManagerFactoryBean {
-//        val em = LocalContainerEntityManagerFactoryBean()
-//        em.dataSource = dataSource
-//        em.setPackagesToScan("fs.playground.entity")
-//        val vendorAdapter: JpaVendorAdapter = HibernateJpaVendorAdapter()
-//        em.jpaVendorAdapter = vendorAdapter
-//        em.setJpaProperties(additionalProperties())
-//        return em
-//    }
-//
-//    @Bean
-//    fun transactionManager(dataSource: DataSource): PlatformTransactionManager? {
-//        val transactionManager = JpaTransactionManager()
-//        transactionManager.entityManagerFactory = entityManagerFactory(dataSource).getObject()
-//        return transactionManager
-//    }
-//
-//    fun additionalProperties(): Properties {
-//        val properties = Properties();
-//        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect")
-//        return properties;
-//    }
-//
-//    class ReplicationRoutingDataSource : AbstractRoutingDataSource() {
-//        override fun determineCurrentLookupKey(): Any? {
-//            return if (Dispatcher.isCurrentTransactionReadOnly()) {
-//                SLAVE_DB_KEY
-//            } else if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) {
-//                SLAVE_DB_KEY
-//            } else {
-//                MASTER_DB_KEY
-//            }
-//        }
-//    }
-//}
+@RestController
+class Ctrl(val psersonService: PersonService) {
+
+    @GetMapping("/complex/readonly")
+    suspend fun complexReadOnlyWithWrite() {
+        psersonService.complexWithReadOnly(UUID.randomUUID().toString())
+    }
+
+    @GetMapping("/complex")
+    suspend fun complexWithWrite() {
+        psersonService.complex(UUID.randomUUID().toString())
+    }
+
+    @GetMapping("/master/read")
+    suspend fun masterRead(): Long {
+        return psersonService.read("master - ${UUID.randomUUID()}")
+    }
+
+    @GetMapping("/master/write")
+    suspend fun masterWrite() {
+        psersonService.write(UUID.randomUUID().toString())
+    }
+
+    @GetMapping("/slave/read")
+    suspend fun readFromSlave(): Long {
+        return psersonService.readFromSlave("slave - ${UUID.randomUUID()}")
+    }
+
+    @GetMapping("/slave/write")
+    suspend fun writeToSlave() {
+        psersonService.writeToSlave(UUID.randomUUID().toString())
+    }
+
+    @GetMapping("/slave/write2")
+    suspend fun writeToSlaveAsReadonl2() {
+        asReadonlyTransaction { psersonService.write(UUID.randomUUID().toString()) }
+    }
+
+    @GetMapping("/slave/readall")
+    suspend fun readAllFromSlave() {
+        psersonService.readAllFromSlave("slaveall - ${UUID.randomUUID()}")
+    }
+
+    @GetMapping("/slave/read/async")
+    suspend fun readFromSlaveAsyc(): Long {
+        val r = psersonService.readFromSlaveAsync(UUID.randomUUID().toString())
+        return r.await()
+    }
+
+}
+
+@Service
+class PersonService(val personRepository: PersonRepository) {
+
+    @Autowired
+    lateinit var _self: PersonService
+
+    suspend fun read(uuid: String) = personRepository.countByName(uuid)
+
+    suspend fun readAsync(uuid: String) = asAsync { personRepository.countByName(uuid) }
+
+    suspend fun readFromSlave(uuid: String) = asReadonlyTransaction { read(uuid) }
+
+    suspend fun readFromSlaveAsync(uuid: String) = asReadonlyTransaction { readAsync(uuid) }
+
+    @Transactional
+    suspend fun write(uuid: String) = personRepository.save(Person(name = uuid))
+
+    @Transactional
+    suspend fun writeToSlave(uuid: String) = asReadonlyTransaction { personRepository.save(Person(name = uuid)) }
+
+    suspend fun readAllFromSlave(uuid: String): Long {
+        return asReadonlyTransaction {
+            val c1 = readAsync(uuid)
+            val c2 = readAsync(uuid)
+            val c3 = readFromSlaveAsync(uuid)
+            val (r1, r2, r3) = awaitAll(c1, c2, c3)
+            r1 + r2 + r3
+        }
+    }
+
+    suspend fun complexWithReadOnly(uuid: String): Long {
+        return asReadonlyTransaction {
+            val c1 = readAsync(uuid)
+            val c2 = readAsync(uuid)
+            val c3 = readFromSlaveAsync(uuid)
+            _self.write(uuid)
+            val (r1, r2, r3) = awaitAll(c1, c2, c3)
+            r1 + r2 + r3
+        }
+    }
+
+    suspend fun complex(uuid: String): Long {
+        val c1 = readAsync(uuid)
+        val c2 = readAsync(uuid)
+        val c3 = readFromSlaveAsync(uuid)
+        _self.write(uuid)
+        val (r1, r2, r3) = awaitAll(c1, c2, c3)
+        return r1 + r2 + r3
+    }
+}
+
+object LTC {
+
+    enum class State {
+        IN_READONLY,
+    }
+
+    private val localThread = ThreadLocal<State?>()
+
+    fun get(): State? {
+        return localThread.get()
+    }
+
+    fun set(value: State?) {
+        localThread.set(value)
+    }
+
+    fun remove() {
+        localThread.remove()
+    }
+
+}
+
+class LTCContext(val message: String? = null) : ThreadContextElement<LTC.State?>, AbstractCoroutineContextElement(LTCContext) {
+    companion object Key : CoroutineContext.Key<LTCContext>
+
+    private val data = LTC.get()
+
+    override fun updateThreadContext(context: CoroutineContext): LTC.State? {
+        val old = LTC.get()
+        LTC.set(data)
+        return old
+    }
+
+    override fun restoreThreadContext(context: CoroutineContext, oldState: LTC.State?) {
+        when (oldState) {
+            LTC.State.IN_READONLY -> LTC.set(oldState)
+            else -> LTC.remove()
+        }
+    }
+}
+
+object LTCDispatcher {
+
+    fun <T> asAsync(call: () -> T): Deferred<T> = CoroutineScope(LTCContext() + Dispatchers.Default).async {
+        call()
+    }
+
+    suspend fun <T> asReadonlyTransaction(call: suspend () -> T): T {
+        val r = CoroutineScope(LTCContext() + Dispatchers.Default).async {
+            withContext(LTCContext("asReadonlyTransaction-safeGuard")) {
+                LTC.set(LTC.State.IN_READONLY)
+                withContext(LTCContext("asReadonlyTransaction")) {
+                    call()
+                }
+            }
+        }
+        return r.await()
+    }
+
+    fun isCurrentTransactionReadOnly() = LTC.get() == LTC.State.IN_READONLY
+}
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = ["fs.playground.repository"],
+        enableDefaultTransactions = false
+)
+@EnableTransactionManagement
+class Config {
+
+    companion object {
+        val SLAVE_DB_KEY = "slave"
+        val MASTER_DB_KEY = "master"
+    }
+
+    @Bean(name = ["masterDataSource"])
+    @ConfigurationProperties(prefix = "master.datasource")
+    fun masterDataSource(): DataSource? {
+        return DataSourceBuilder.create().type(HikariDataSource::class.java).build()
+    }
+
+    @Bean(name = ["slaveDataSource"])
+    @ConfigurationProperties(prefix = "slave.datasource")
+    fun slaveDataSource(): DataSource? {
+        return DataSourceBuilder.create().type(HikariDataSource::class.java).build()
+    }
+
+    @Bean(name = ["routingDataSource"])
+    fun routingDataSource(
+            @Qualifier("masterDataSource") masterDataSource: DataSource,
+            @Qualifier("slaveDataSource") slaveDataSource: DataSource,
+    ): DataSource {
+        val routingDataSource = ReplicationRoutingDataSource()
+        val dataSourceMap: MutableMap<Any, Any> = HashMap()
+        dataSourceMap[MASTER_DB_KEY] = masterDataSource
+        dataSourceMap[SLAVE_DB_KEY] = slaveDataSource
+        routingDataSource.setTargetDataSources(dataSourceMap)
+        routingDataSource.setDefaultTargetDataSource(masterDataSource)
+        return routingDataSource
+    }
+
+    @Primary
+    @Bean
+    fun dataSource(@Qualifier("routingDataSource") routingDataSource: DataSource): DataSource? {
+        return LazyConnectionDataSourceProxy(routingDataSource)
+    }
+
+    @Bean
+    fun entityManagerFactory(dataSource: DataSource): LocalContainerEntityManagerFactoryBean {
+        val em = LocalContainerEntityManagerFactoryBean()
+        em.dataSource = dataSource
+        em.setPackagesToScan("fs.playground.entity")
+        val vendorAdapter: JpaVendorAdapter = HibernateJpaVendorAdapter()
+        em.jpaVendorAdapter = vendorAdapter
+        em.setJpaProperties(additionalProperties())
+        return em
+    }
+
+    @Bean
+    fun transactionManager(dataSource: DataSource): PlatformTransactionManager? {
+        val transactionManager = LTCJpaTransactionManager()
+        transactionManager.entityManagerFactory = entityManagerFactory(dataSource).getObject()
+        return transactionManager
+    }
+
+    fun additionalProperties(): Properties {
+        val properties = Properties();
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect")
+        return properties;
+    }
+
+    class ReplicationRoutingDataSource : AbstractRoutingDataSource() {
+        override fun determineCurrentLookupKey(): Any? {
+//            println("routing: ${LTCDispatcher.isCurrentTransactionReadOnly()} , ${TransactionSynchronizationManager.isCurrentTransactionReadOnly()}")
+            return when {
+                LTCDispatcher.isCurrentTransactionReadOnly() -> {
+                    SLAVE_DB_KEY
+                }
+                TransactionSynchronizationManager.isCurrentTransactionReadOnly() -> {
+                    SLAVE_DB_KEY
+                }
+                else -> {
+                    MASTER_DB_KEY
+                }
+            }
+        }
+    }
+
+    class LTCJpaTransactionManager : JpaTransactionManager() {
+        override fun doCommit(status: DefaultTransactionStatus) {
+//            println("commit: ${LTCDispatcher.isCurrentTransactionReadOnly()} , ${TransactionSynchronizationManager.isCurrentTransactionReadOnly()}")
+            if (LTCDispatcher.isCurrentTransactionReadOnly()) {
+                throw Exception("readonly transaction")
+            } else {
+                super.doCommit(status)
+            }
+        }
+    }
+}
