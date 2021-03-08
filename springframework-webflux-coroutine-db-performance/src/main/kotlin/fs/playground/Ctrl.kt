2be2ec060edb26732262f0c139bf43bee3c94097
freestrings <freestrings@gmail.com>
2021-03-08 23:50:52 +0900

    mvc

--- springframework-webflux-coroutine-db-performance/src/main/kotlin/fs/playground/Ctrl.kt
+++ springframework-webflux-coroutine-db-performance/src/main/kotlin/fs/playground/Ctrl.kt
@@ -1,14 +1,9 @@
 package fs.playground
 
-import kotlinx.coroutines.*
-import kotlinx.coroutines.reactor.asCoroutineDispatcher
 import org.springframework.jdbc.core.JdbcTemplate
-import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import reactor.core.scheduler.Schedulers
-import java.util.*
 
 @RestController
 class Ctrl(val jdbcTemplate: JdbcTemplate, val personRepository: PersonRepository) {
@@ -62,19 +57,19 @@ class Ctrl(val jdbcTemplate: JdbcTemplate, val personRepository: PersonRepositor
     private fun query(type: String): List<Person> {
         return when (type) {
             "jpaNative" -> {
-                println("jpaNative")
+                println("jpaNative ${Thread.currentThread()}")
                 jpaNative()
             }
             "jpql" -> {
-                println("jpql")
+                println("jpql ${Thread.currentThread()}")
                 jpql()
             }
             "jpa" -> {
-                println("jpa")
+                println("jpa ${Thread.currentThread()}")
                 jpa().toList()
             }
             else -> {
-                println("jdbc")
+                println("jdbc ${Thread.currentThread()}")
                 jdbc()
             }
         }
@@ -84,79 +79,8 @@ class Ctrl(val jdbcTemplate: JdbcTemplate, val personRepository: PersonRepositor
      * 기본 워커에서 동작하고, 워커스레드 개수 확 늘어남
      */
     @GetMapping("/io")
-    suspend fun io(@RequestParam("type", required = false, defaultValue = "jdbc") type: String): List<Person> =
-        coroutineScope {
-            val a = async(Dispatchers.IO + TestCtx(UUID.randomUUID().toString())) {
-                println("IO - ${Thread.currentThread().name}")
-                query(type)
-            }
-            a.await()
-        }
+    fun io(@RequestParam("type", required = false, defaultValue = "jdbc") type: String): List<Person> = query(type)
 
-    /**
-     * epoll 스레드에서 동작
-     */
-    @GetMapping("/default")
-    suspend fun default(@RequestParam("type", required = false, defaultValue = "jdbc") type: String): List<Person> =
-        coroutineScope {
-            val a = async(TestCtx(UUID.randomUUID().toString())) {
-                println("Default - ${Thread.currentThread().name}")
-                query(type)
-            }
-            a.await()
-        }
-
-    /**
-     * 기본 워커에서 동작하고
-     * -Dkotlinx.coroutines.scheduler.core.pool.size 로 풀 크기가 조절됨
-     */
-    @GetMapping("/global")
-    fun global(
-        @RequestParam(
-            "type",
-            required = false,
-            defaultValue = "jdbc"
-        ) type: String
-    ): Deferred<List<Person>> =
-        GlobalScope.async(TestCtx(UUID.randomUUID().toString())) {
-            println("Global - ${Thread.currentThread().name}")
-            query(type)
-        }
-
-    val POOL = Schedulers.newBoundedElastic(10, 100000, "pool").asCoroutineDispatcher()
-
-    /**
-     * 지정한 풀에서 동작됨
-     */
-    @GetMapping("/bounded")
-//    @Transactional(readOnly = true)
-    suspend fun bounded(@RequestParam("type", required = false, defaultValue = "jdbc") type: String): List<Person> =
-        coroutineScope {
-            val a = async(POOL + TestCtx(UUID.randomUUID().toString())) {
-                println("BOUNDED - $type - ${Thread.currentThread().name}")
-                query(type)
-            }
-            a.await()
-        }
-
-    /**
-     * 지정한 풀에서 동작됨
-     */
-    @GetMapping("/bounded2")
-    suspend fun bounded2(
-        @RequestParam(
-            "type",
-            required = false,
-            defaultValue = "jdbc"
-        ) type: String
-    ): List<Person> {
-        val a = CoroutineScope(POOL + TestCtx(UUID.randomUUID().toString())).async {
-            println("BOUNDED2 - ${Thread.currentThread().name}")
-            query(type)
-        }
-
-        return a.await()
-    }
 
     @GetMapping("/loop")
     suspend fun deferredLoop(
@@ -164,14 +88,7 @@ class Ctrl(val jdbcTemplate: JdbcTemplate, val personRepository: PersonRepositor
         @RequestParam("type") type: String,
         @RequestParam("queryType", required = false, defaultValue = "jpa") queryType: String
     ): String {
-        when (type) {
-            "io" -> (0..count).map { io(queryType) }
-            "bounded" -> (0..count).map { bounded(queryType) }
-            "bounded2" -> (0..count).map { bounded2(queryType) }
-            "global" -> awaitAll(*(0..count).map { global(queryType) }.toTypedArray())
-            else -> (0..count).map { default(queryType) }
-        }
-
+        (0..count).map { io(queryType) }
         return "ok"
     }
 }
