8a282262e4559f43dc792b658be13eea5865a1ea
freestrings <freestrings@gmail.com>
2022-02-10 20:43:00 +0900

    mha 복제수정3

--- mha/res/slave/db-shl/00-init-db.sh
+++ mha/res/slave/db-shl/00-init-db.sh
@@ -3,6 +3,8 @@
 mysql -uroot -p1234 <<END
     CREATE USER 'mha'@'%' identified by '1234';
     GRANT ALL PRIVILEGES ON *.* TO 'mha'@'%';
+    CREATE USER 'rep'@'%' identified by '1234';
+    GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* to 'rep'@'%';
     FLUSH PRIVILEGES;
 END
 
